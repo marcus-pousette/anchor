a32644a29be74d544e23cab382f7158ce679901a
Marcus Pousette <marcus.pousette@quantleaf.com>
2025-09-01 13:46:11 +0200

    fix: revert

--- lang/syn/src/idl/defined.rs
+++ lang/syn/src/idl/defined.rs
@@ -497,7 +497,8 @@ pub fn gen_idl_type(
 
                 // If no path was found, just return an empty path and let the find_path function handle it
                 let source_path = proc_macro2::Span::call_site()
-                    .source_file()
+                    .unwrap()
+                    .local_file()
                     .unwrap_or_default();
 
                 if let Ok(Ok(ctx)) = find_path("lib.rs", &source_path).map(CrateContext::parse) {
